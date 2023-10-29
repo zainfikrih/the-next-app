d5f7d597f728d9e081b863832e4edb9c1702a08c
zainfikrih <zainfikrih@gmail.com>
2023-10-29 14:13:11 +0700

    fixing defaultColorScheme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -66,7 +66,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <ColorSchemeScript />
+        <ColorSchemeScript defaultColorScheme="dark" />
         <ThemeProvider />
       </head>
       <body className={inter.className}>
