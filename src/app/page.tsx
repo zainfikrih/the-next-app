e8e32a1d7c8c8b436fc9208a88f41d88b4bf6dfb
zainfikrih <zainfikrih@gmail.com>
2023-10-29 13:59:32 +0700

    improvement app page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import { Center, Loader } from '@mantine/core'
-import { redirect } from 'next/navigation'
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  redirect('/home')
-  return (
-    <Center h={'100vh'}>
-      <Loader />
-    </Center>
-  )
+  redirect("/home");
 }
